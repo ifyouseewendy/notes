f21386fa2c08a2add6179eea46615c3d4692897c
wendi <wendi@umeng.com>
2012-08-25 02:10:42 +0800

    added dispatch_me example in chap4(controller)

---
+++ the_rails3_way/dispatch_me/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DispatchMe::Application.config.secret_token = '7ea9c1702cf4dc2adced4f977372a646c17f3a88ee11e34077ef914f7ddedfa2026eab240b422a8ecd531721919b0a02d0a4b2ef1b4fc33f2b31f902b52a3469'
